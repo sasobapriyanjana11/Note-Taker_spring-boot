6c65a2712c728b1d9e065efb614fca60e43fe421
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-06 14:01:44 +0530

    db and hibernate configuration

--- src/main/java/lk/ijse/gdse/aad68/notetaker/dao/UserDao.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/dao/UserDao.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse.aad68.notetaker.dao;
 
 import lk.ijse.gdse.aad68.notetaker.entity.UserEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
